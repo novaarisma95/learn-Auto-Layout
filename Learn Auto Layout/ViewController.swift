8a4de85224cb978ebe27d0fe3356ca7e5ea3662f
Nova Arisma <novaarisma95@gmail.com>
2019-12-03 15:08:15 +0700

    Initial Commit

---
+++ Learn Auto Layout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Learn Auto Layout
+//
+//  Created by Nova Arisma on 12/3/19.
+//  Copyright © 2019 Nova Arisma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
